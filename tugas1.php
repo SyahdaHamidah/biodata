a35c8bfcc34e03b690be346e17e7957e2dfd0d16
syahdahhamidah123 <63383939+syahdahhamidah123@users.noreply.github.com>
2020-04-09 11:28:40 +0700

    add biodata

--- tugas1.php
+++ tugas1.php
@@ -2,5 +2,6 @@
 echo'Nama: Syahdah Hamidah<br>';
 echo'Kelas: XI RPL 1<br>';
 echo'Asal sekolah: SMK Taruna Bhakti<br>';
-echo'Alamat rumah: Gg H Damon 5';
+echo'No Hp: 088977103568<br>';
+echo'Email: syahdah121@gmail.com';
 ?>
